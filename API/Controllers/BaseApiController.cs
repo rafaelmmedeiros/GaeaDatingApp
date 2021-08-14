386e6b260cd8031571c91121a3461f4a08de55cd
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-08-14 13:12:07 -0300

    Auth

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+
+    // Atribute APIController vlidade parameters
     [ApiController]
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
