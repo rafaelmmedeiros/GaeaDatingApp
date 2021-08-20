ea913c0ae8d51806ee56a858ddeca22a96608512
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-08-20 14:41:55 -0300

    client register

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -13,29 +13,14 @@ export class AppComponent implements OnInit {
   users: any;
 
   // Dependecy Injection
-  constructor(
-    private http: HttpClient,
-    private accountService: AccountService
-  ) {}
+  constructor(private accountService: AccountService) {}
 
   ngOnInit() {
-    this.getUsers();
     this.setCurrentUser();
   }
 
   setCurrentUser() {
     const user: User = JSON.parse(localStorage.getItem('user'));
     this.accountService.setCurrentUser(user);
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe(
-      (response) => {
-        this.users = response;
-      },
-      (error) => {
-        console.log(error);
-      }
-    ); // Lazy Loading needs someone watching to do something... they are like "PIÂO DE FIRMA"
   }
 }
