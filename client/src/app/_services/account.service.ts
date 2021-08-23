6a5b2a0603cce58cd34f548ac74d66bc362c55b1
Rafael Medeiros <rafael.einherjar@gmail.com>
2021-08-23 16:27:54 -0300

    client register

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -10,21 +10,31 @@ import { User } from '../_models/user';
 })
 export class AccountService {
   baseUrl = 'https://localhost:5001/api/';
-  //  ReplaySubjects work as a buffer object
-  private currenUserSource = new ReplaySubject<User>(1);
+  private currenUserSource = new ReplaySubject<User>(1); //  ReplaySubjects work as a buffer object
   currentUser$ = this.currenUserSource.asObservable();
 
   constructor(private http: HttpClient) {}
 
   login(model: any) {
     return this.http.post(this.baseUrl + 'account/login', model).pipe(
-      //  RxJS operator https://rxjs.dev/
       map((response: User) => {
-        const user = response;
+        const user = response; //  RxJS operator https://rxjs.dev/
         if (user) {
           localStorage.setItem('user', JSON.stringify(user));
           this.currenUserSource.next(user);
         }
+      })
+    );
+  }
+
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'account/register', model).pipe(
+      map((user: User) => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currenUserSource.next(user);
+        }
+        //return user;
       })
     );
   }
